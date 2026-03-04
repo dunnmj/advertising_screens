e807994f662246c9b9206573f5561964b5650ac4
dunnmj <dunnmj@users.noreply.github.com>
2026-03-04 21:29:51 +0000

    feat: update Django version and use unfold for nicer admin interface

--- advertising/base_settings.py
+++ advertising/base_settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
+    'unfold.contrib.filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +47,89 @@ INSTALLED_APPS = [
     'admin_ordering',
     'django_cleanup.apps.CleanupConfig',  # TODO will need to detect image load failure and reload page if it occurs
 ]
+
+UNFOLD = {
+    "SITE_TITLE": "Display Screen Admin",
+    "SITE_HEADER": "Display Screen Admin",
+    "SITE_URL": "/",
+    "SHOW_HISTORY": True,
+    "SHOW_VIEW_ON_SITE": True,
+    "SIDEBAR": {
+        "show_search": True,
+        "show_all_applications": False,
+        "navigation": [
+            {
+                "title": "Screens & Playlists",
+                "separator": False,
+                "items": [
+                    {
+                        "title": "Screens",
+                        "icon": "monitor",
+                        "link": reverse_lazy("admin:screens_screen_changelist"),
+                    },
+                    {
+                        "title": "Playlists",
+                        "icon": "queue_play_next",
+                        "link": reverse_lazy("admin:screens_playlist_changelist"),
+                    },
+                    {
+                        "title": "Sources",
+                        "icon": "perm_media",
+                        "link": reverse_lazy("admin:screens_source_changelist"),
+                    },
+                ],
+            },
+            {
+                "title": "Scheduling",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Schedules",
+                        "icon": "calendar_today",
+                        "link": reverse_lazy("admin:screens_schedule_changelist"),
+                    },
+                    {
+                        "title": "Schedule Rules",
+                        "icon": "rule",
+                        "link": reverse_lazy("admin:screens_schedulerule_changelist"),
+                    },
+                ],
+            },
+            {
+                "title": "Room Schedules",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Venues",
+                        "icon": "location_on",
+                        "link": reverse_lazy("admin:room_schedules_venue_changelist"),
+                    },
+                    {
+                        "title": "Rooms",
+                        "icon": "meeting_room",
+                        "link": reverse_lazy("admin:room_schedules_room_changelist"),
+                    },
+                ],
+            },
+            {
+                "title": "Users & Groups",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Users",
+                        "icon": "person",
+                        "link": reverse_lazy("admin:auth_user_changelist"),
+                    },
+                    {
+                        "title": "Groups",
+                        "icon": "group",
+                        "link": reverse_lazy("admin:auth_group_changelist"),
+                    },
+                ],
+            },
+        ],
+    },
+}
 
 CRON_CLASSES = [
     "screens.cron.CleanUpSources",
--- room_schedules/admin.py
+++ room_schedules/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
 from room_schedules.models import Venue, Room
 
-admin.site.register(Venue)
-admin.site.register(Room)
+
+@admin.register(Venue)
+class VenueAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Room)
+class RoomAdmin(ModelAdmin):
+    pass
