b9b435490e242850be5af86ae7b0b44a48d6d38f
dunnmj <dunnmj@users.noreply.github.com>
2026-03-07 18:01:13 +0000

    feat: add O365 integration with event fetching and update models for O365 support

--- room_schedules/admin.py
+++ room_schedules/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, TabularInline
 
 from room_schedules.models import Venue, Room
 
 
+class RoomInline(TabularInline):
+    model = Room
+    extra = 0
+    fields = ('name', 'artifax_id', 'o365_calendar_email')
+
+
 @admin.register(Venue)
 class VenueAdmin(ModelAdmin):
-    pass
+    list_display = ('name', 'artifax_id')
+    search_fields = ('name',)
+    inlines = [RoomInline]
 
 
 @admin.register(Room)
 class RoomAdmin(ModelAdmin):
-    pass
+    list_display = ('name', 'venue', 'artifax_id', 'o365_calendar_email')
+    search_fields = ('name', 'venue__name')
+    list_filter = ('venue',)
+    list_select_related = ('venue',)
